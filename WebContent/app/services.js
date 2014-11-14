0b09288ba490ae3737ae79fea5283df39f916f75
Aleksander Bai <ab@prognett.no>
2014-11-14 23:27:51 +0100

    Removed old stuff

--- WebContent/app/services.js
+++ WebContent/app/services.js
@@ -13,39 +13,3 @@ app.service('RestService', ['$http', 'varRestBase', function ($http, varRestBase
     }
 }]
 );
-
-app.factory('AuthFactory', ['RestService', function (RestService) {
-    var credentials = {};
-    window.credentials = credentials;
-    return {
-        reset: function () {
-            credentials.username = '';
-            credentials.password = '';
-            return credentials;
-        },
-        login: function (loginCredentials) {
-            credentials.username = (loginCredentials && loginCredentials.hasOwnProperty('username')) ? loginCredentials.username : null;
-            credentials.password = (loginCredentials && loginCredentials.hasOwnProperty('password')) ? loginCredentials.password : null;
-            return this.authInREST();
-        },
-        authInREST: function () {
-            return RestService.post('auth/' + credentials.username + '.json');   // this called mocked json response by username.
-        }
-    }
-}]
-);
-
-
-app.factory('TranslationLoaderService',['RestService','$q', function (RestService, $q) {
-    return function (options) {
-        var deferred = $q.defer();
-        var restCall = RestService.get('i18n/locale-' + options.key + '.json');
-        restCall.success(function (data) {
-            deferred.resolve(data);
-        });
-        restCall.error(function () {
-            deferred.reject(options.key);
-        });
-        return deferred.promise;
-    };
-}]) ;
