337713f1904e77c69ee9ba0d1a4a8cab54ba3b39
Alex <ab@prognett.no>
2014-11-16 21:39:06 +0100

    adjusted logging again

--- src/main/java/no/home/automation/rfxcom/RFXComSerialConnector.java
+++ src/main/java/no/home/automation/rfxcom/RFXComSerialConnector.java
@@ -23,7 +23,7 @@ import org.slf4j.LoggerFactory;
 
 public class RFXComSerialConnector implements RFXComConnectorInterface
 {
-	private static final Logger					logger			= LoggerFactory.getLogger("stdoutLogger");
+	private static final Logger					logger			= LoggerFactory.getLogger("fileLogger");
 
 	private static List<RFXComEventListener>	_listeners		= new ArrayList<RFXComEventListener>();
 
@@ -143,7 +143,7 @@ public class RFXComSerialConnector implements RFXComConnectorInterface
 				{
 
 					byte[] logData = Arrays.copyOf(tmpData, len);
-					logger.debug("Received data (len={}): {}", len, DatatypeConverter.printHexBinary(logData));
+					logger.trace("Received data (len={}): {}", len, DatatypeConverter.printHexBinary(logData));
 
 					for (int i = 0; i < len; i++)
 					{
